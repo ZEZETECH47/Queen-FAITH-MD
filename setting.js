e3b141a04d904306638f591ce758ee862d9eb96f
ZEZETECH47 <humphreymbise75@gmail.com>
2025-09-30 23:55:27 +0300

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'queen_faith-md',
+  author: '‎',
+  botName: "FAITH-MD͎",
+  botOwner: '👑kin ZEZE👑', // Your name
+  ownerNumber: '255747397675', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+};
+
+module.exports = settings;
